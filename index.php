fb81677d1cc77aa2a71523158111c4f59c27c74d
Christopher C. Charles <cccharle@subplex.com>
2015-03-14 15:19:47 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<!--
+<
+<            PIXELS FORGED BY:
+<                                       @@@@                        @@@@                   
+<                                      @@@@                        @@@@                   
+<                                     @@@@                        @@@@                    
+<                                    @@@@                        @@@@                    
+<       @@@@@@@@@@@@  @@@@    @@@@  @@@@@@@@@@@@  @@@@@@@@@@@@  @@@@  @@@@@@@@@@@@  @@@@   @@@@ 
+<      @@@@          @@@@    @@@@  @@@@    @@@@  @@@@    @@@@  @@@@  @@@@    @@@@    @@@@@@@  
+<     @@@@    @@@@  @@@@    @@@@  @@@@    @@@@  @@@@    @@@@  @@@@  @@@@    @@@@      @@@@  
+<            @@@@  @@@@    @@@@  @@@@    @@@@  @@@@    @@@@  @@@@  @@@@            @@@@@@@@   
+<   @@@@@@@@@@@@  @@@@@@@@@@@@  @@@@@@@@@@@@  @@@@@@@@@@@@  @@@@  @@@@@@@@@@@@   @@@@   @@@@  
+<                                            @@@@  
+<                                           @@@@   S  T  R  A  T  E  G  I  C           
+<                                          @@@@   C  O  M  M  U  N  I  C  A  T  I  O  N           
+<                                         @@      
+<                                                   
+<                                                   
+<                                            http://subplex.com
+<                                               
+<-->
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=medium-dpi" />
+
+        <title>Busta Meme!</title>
+
+
+        <!--    /////////////////       -->
+        <!--    // STYLESHEETS //       -->
+        <!--    /////////////////       -->
+        <link rel="stylesheet" type="text/css" href="css/reset.css" media="all" />
+        <link rel="stylesheet" type="text/css" href="css/sx_reset.css" />
+        <link rel="stylesheet" type="text/css" href="css/mbj_capsule.css" />
+        <link rel="stylesheet" type="text/css" href="css/mbjNotifier.css" media="all" />
+        <link rel="stylesheet" type="text/css" href="css/global.css" media="all" />
+        <link rel="stylesheet" type="text/css" href="css/devices.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="css/jquery.jqpuzzle.css" media="all" />
+        
+        
+        <!--    ///////////////         -->
+        <!--    //  SCRIPTS  //         -->
+        <!--    ///////////////         -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>     
+        <script type="text/javascript" charset="utf-8" src="lib/fastclick/lib/fastclick.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/js/mbjNotifier.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/js/mbjCredentials.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/mybeanjar/code/MBJRequest.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/mybeanjar/code/sdk/apiRequest.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/mybeanjar/code/sdk/md5.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/mybeanjar/code/sdk/appsMethods.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/mybeanjar/code/sdk/winnersMethods.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/mybeanjar/code/sdk/sponsorsMethods.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/mybeanjar/code/sdk/sponsorLocationMethods.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/mybeanjar/code/sdk/beansMethods.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/mybeanjar/code/sdk/categoriesMethods.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/mybeanjar/code/sdk/awardMethods.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/mybeanjar/code/sdk/registerMethods.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/mybeanjar/code/sdk/sendpasswordMethods.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/mybeanjar/code/sdk/deleteMethods.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/mybeanjar/code/sdk/reedemMethods.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/mybeanjar/code/sdk/authenticateuserMethods.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/mybeanjar/code/sdk/validateuserMethods.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/js/spin.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/js/jquery.ba-dotimeout.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/js/jquery.color-2.1.2.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/js/jquery-animate-css-rotate-scale.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/js/sxHelpers.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/js/jquery.jqpuzzle.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/js/slider/jssor.core.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/js/slider/jssor.utils.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/js/slider/jssor.slider.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/js/winners.js"></script>
+        <script type="text/javascript" charset="utf-8" src="lib/js/slider.js"></script>
+        <script type="text/javascript">
+            jQuery(document).ready(function() {
+                initJQPuzzle();
+                console.log("Puzzle ready.");
+                mbjAttemptAward();
+                get_winners("mainmenu");
+            });
+        </script>
+    </head>
+   
+
+
+    <body>
+        <div id="app">
+
+            <div id="puzzle-page" data-role="page" class="pg-page">
+                <div data-role="header" data-id="header" id="header" data-position="fixed" class="ui-header">
+                </div>
+                <div data-role="content" class="content content-puzzle">
+                    
+                    <div class="container" id="puzz-outer">
+                        <img src="img/bustameme-title.png" class="all-columns" id="bam-title">
+                        <div class="container puzz-element" id="puzzle-container">
+                            <?php 
+                                $dir = "img/puzz/";
+                                $imgpool = scandir($dir);
+                                $randomimg = rand(2, sizeof($imgpool)-1);
+                            ?>
+                            <img class="jqPuzzle jqp-r3-c3-h1-SNABCDE" id="puzzImg" src="img/puzz/<?php echo $imgpool[$randomimg]; ?>">
+                            <img id="hintbutton" onclick="javascript:updateHint()" src="img/hint-btn-full.png">
+                        </div>
+                    </div>
+               
+                    <div class="mbj-footer">
+                        <div class="footer-header-strip">
+                            <span class="footer-header-strip-counter count_wins">Thousands of Beans awarded</span>
+                        </div>
+                        <div id="main_container" class="panel_container">
+                            <div class="slider_temp">
+                                <div id="slider1_container">
+                                    <div u="slides" id="slider_components">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="footer-signup-strip">
+                            <div id="mbj-footer-logo">
+                                <a href="http://mybeanjar.com" target="_blank"><img src="img/mbj_logo_sm_50px.png" id="footer-logo"></a>
+                            </div>
+                            <div id="mbj-footer-cta">
+                                <div id="mbj-footer-cta-sub">
+                                    <span class="footer-cta-title">Play, Win, Cash-In!</span>
+                                    <button id="footer-signup" onclick="javascript:mbjAttemptAward()">Sign Up</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <!-- BEAN NOTIFICATION WINDOW -->
+                <div class="bean_notification_window">
+                    <img class="bean_notification_image" src="">
+                    <p class="bean_notification_text">...</p>
+                </div>
+
+
+
+                <!-- BEAN AWARD CAPSULE MODAL -->
+                <div class="mbj_capsule_container autocentered hidden">
+                    <div class="mbj_capsule mbj_anim_capsule_init">
+                        <img class="mbj_capsule_body" src="img/mbj_capsule_body.png">
+                        <img class="mbj_capsule_lid" src="img/mbj_capsule_lid.png">
+                        <div class="mbj_capsule_payload mbj_anim_halfscale">
+                            <img class="mbj_capsule_payload_contents" id="mbj_payload_overlay" src="img/mbj_payload_overlay.jpg">
+                            <img class="mbj_capsule_payload_contents" id="mbj_award_img" src="img/mbj_payload_img.png">
+                            <img class="mbj_capsule_glory" src="img/mbj_payload_glory.png">
+                        </div>
+
+                    </div>
+                </div>
+
+
+
+                <!-- MBJ LOGIN/REGISTRATION MODAL -->
+                <div class="mbj_notification_container autocentered" id="mbj_notification_container_login">
+                    <div class="mbj_notification_title">
+                        <img src="img/mbj_logo_50px.png" />
+                        <h2>Play, Win, Cash In NOW</h2>
+                    </div>
+                    <div class="mbj_notification_copy">
+                        <p>MyBeanJar scores great deals on stuff YOU want at the right time &amp; place. Real brands instantly, not points.</p>
+                        <p>Win for Real Now!</p>
+                    </div>
+
+                    <form id="mbj_notification_close">
+                        <button type="submit" class="btn-close" id="mbj_notification_close_submit">
+                            <img src="img/ui_action_close.png">
+                        </button>
+                    </form>
+                    
+                    <div class="mbj_notification_pane_selectors">
+                        <div class="mbj_notification_pane_selector active_selector">
+                            <h2>Login</h2>
+                        </div>
+                        <div class="mbj_notification_pane_selector inactive_selector">
+                            <h2>Sign Up</h2>
+                        </div>
+                    </div>
+
+                    <div class="mbj_notification_pane mbj_login_or_register active_pane" id="mbj_login">
+                        <form class="formoid-default-skyblue" id="mbj_login_form" method="post">
+                            <div class="element-input">
+                                <label class="title" style="color: #a9a9a9;">username</label>
+                                <input class="large" id="mbj_form_u" type="text" name="mbj_login" required/>
+                            </div>
+                            <div class="element-input">
+                                <label class="title" style="color: #a9a9a9;">password</label>
+                                <input class="large" id="mbj_form_p" type="password" name="mbj_password" required/>
+                            </div>
+                            <div class="mbj_login_status">
+                            </div>
+                            <div class="submit">
+                                <button type="submit" class="btn-mbj submit full-width" id="mbj_login_submit">
+                                    <span><i class="ion-log-in" style="font-size: 28px; vertical-align: middle; padding-right:0.5em;"></i>Login</span>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div class="mbj_notification_pane mbj_login_or_register inactive_pane" id="mbj_register">
+                        <form class="formoid-default-skyblue" id="mbj_registration_form" method="post">
+                            <div class="element-input">
+                                <label class="title" style="color: #a9a9a9;">username</label>
+                                <input class="large" id="mbj_form_reg_u" type="text" name="mbj_reg_username" required/>
+                            </div>
+                            <div class="element-input">
+                                <label class="title" style="color: #a9a9a9;">password</label>
+                                <input class="large" id="mbj_form_reg_p" type="password" name="mbj_reg_password" required/>
+                            </div>
+                            <div class="element-input">
+                                <label class="title" style="color: #a9a9a9;">confirm password</label>
+                                <input class="large" id="mbj_form_reg_p2" type="password" name="mbj_reg_passconfirm" required/>
+                            </div>
+                            <div class="element-input">
+                                <label class="title" style="color: #a9a9a9;">e-mail</label>
+                                <input class="large" id="mbj_form_reg_email" type="email" name="mbj_reg_email" required/>
+                            </div>
+                            <div class="element-input">
+                                <label class="title" style="color: #a9a9a9;">ZIP</label>
+                                <input class="large" id="mbj_form_reg_zip" type="text" name="mbj_reg_zip" required/>
+                            </div>
+                            <div class="element-input">
+                                <label class="title" style="color: #a9a9a9;">Bean reward categories (select 3)</label>
+                                <input class="mbj_checkbox" type="checkbox" value="Apparel"><span class="form-checkbox">Apparel</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Automotive"><span class="form-checkbox">Automotive</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Food and Beverage"><span class="form-checkbox">Food and Beverage</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Personal Care"><span class="form-checkbox">Personal Care</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Health and Fitness"><span class="form-checkbox">Health and Fitness</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Entertainment"><span class="form-checkbox">Entertainment</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Flowers and Gifts"> <span class="form-checkbox">Flowers and Gifts</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Grocery or Packaged Goods"><span class="form-checkbox">Grocery or Packaged Goods</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Home and Garden"><span class="form-checkbox">Home and Garden</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Pets"><span class="form-checkbox">Pets</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Tech"><span class="form-checkbox">Tech</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Travel and Leisure"><span class="form-checkbox">Travel and Leisure</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Office"><span class="form-checkbox">Office</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Grab Bag"><span class="form-checkbox">Grab Bag</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Charitable Donation"><span class="form-checkbox">Charitable Domation</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Other"><span class="form-checkbox">Other</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Education"><span class="form-checkbox">Education</span>
+                                <input class="mbj_checkbox" type="checkbox" value="Games"><span class="form-checkbox">Games</span>
+                            </div>
+                            <div class="mbj_login_status">
+                            </div>
+                            <div class="submit">
+                                <button type="submit" class="btn-mbj submit full-width" id="mbj_registration_submit">
+                                    <span><i class="ion-clipboard" style="font-size: 28px; vertical-align: middle; padding-right:0.5em;"></i>Sign Up</span>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
